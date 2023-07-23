4096b95081e75f3c37384f6c40d2b34dd4e35b08
Sam <semernnaduev@gmail.com>
2023-07-23 14:52:01 +0200

    Added lesson's code

--- app/src/main/java/com/example/mutlithreading/MainActivity.kt
+++ app/src/main/java/com/example/mutlithreading/MainActivity.kt
@@ -3,36 +3,74 @@ package com.example.mutlithreading
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.Exception
+
+private const val TAG = "TESTTAG"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val thread1 = MyThread()
-        val thread2 = MyThread()
-        val thread3 = MyThread()
-        thread1.start()
-        thread2.start()
-        thread3.start()
+        val store = Store()
+        Thread(Producer(store)).start()
+        Thread(Buyer(store)).start()
     }
 
-    class MyRunnable:Runnable{
+    class Store : Object() {
 
-        override fun run() {
-            Log.d("TESTTAG", "Hello from ${Thread.currentThread().name}")
+        private var productCount: Int = 0
+
+        @Synchronized
+        fun buy() {
+            while (productCount == 0) {
+                try {
+                    wait()
+                } catch (_: Exception) {
+
+                }
+            }
+            productCount--
+            Log.d(TAG, "Buyer bought 1 good")
+            Log.d(TAG, "Products count = $productCount")
+            notify()
+        }
+
+        @Synchronized
+        fun put() {
+            while (productCount >= 3) {
+                try {
+                    wait()
+                } catch (_: Exception) {
+
+                }
+            }
+
+            productCount++
+            Log.d(TAG, "Producer delivered 1 good")
+            Log.d(TAG, "Products count = $productCount")
+            notify()
         }
 
     }
 
-    class MyThread : Thread() {
+    class Buyer(private val store: Store) : Object(), Runnable {
 
         override fun run() {
-            super.run()
-            for (i in 0..4){
-                sleep(500)
-                Log.d("TESTTAG", "I = $i")
+            for (i in 0..5) {
+                store.buy()
             }
         }
+
     }
+
+    class Producer(private val store: Store) : Object(), Runnable {
+
+        override fun run() {
+            for (i in 0..5) {
+                store.put()
+            }
+        }
+
+    }
+
+
 }
